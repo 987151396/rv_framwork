1ff74394f881c061f3400468fedfabd36435b4aa
ouyangyue <987151396@qq.com>
2026-05-25 17:26:42 +0800

    Initialize RvFramework and add FlowBus events for volume and screensaver

--- app/src/main/java/com/rv/framwork/cmd/base/BaseCommand.kt
+++ app/src/main/java/com/rv/framwork/cmd/base/BaseCommand.kt
@@ -1,47 +1,44 @@
 package com.rv.framwork.cmd.base
 
-import com.rv.framwork.event.EventLiveData
+import androidx.lifecycle.MutableLiveData
 
-abstract class BaseCommand : Command
-{
-    var dataHandler : DataHandler? = null
+abstract class BaseCommand : Command {
+    var dataHandler: DataHandler? = null
 
-    val isCanBusUpdate = EventLiveData<Boolean>().apply {
-        this.value = false
-    }
+    // 修改为 MutableLiveData 并设置初始值，以便后续可以修改其 value
+    val isCanBusUpdate = MutableLiveData<Boolean>(false)
 
     //protected var portStr = "/dev/ttyS7"
     //protected var portStr = "/dev/ttyS4"
     //protected var portStr = "/dev/ttyHS1"//讯族
     protected var portStr = "/dev/ttyS1"
-    companion object{
+
+    companion object {
         //var ibaudRate = 38400
         var ibaudRate = 115200
     }
 
     override fun sendCom2Port(data: ByteArray) {
-        sendCom2Port(data,data.size)
+        sendCom2Port(data, data.size)
     }
 
     override fun sendCom2Port(data: ByteArray, size: Int) {
-
     }
 
-    open fun isCanBusUpdate(values : Boolean){
+    open fun isCanBusUpdate(values: Boolean) {
         isCanBusUpdate.value = values
     }
 
     abstract fun exit()
-    abstract fun start():Int
+    abstract fun start(): Int
 
     interface DataHandler {
         fun onDataReceived(comName: String?, buffer: ByteArray?, size: Int)
         fun onDataSend(buffer: ByteArray?)
     }
-
 }
 
-enum class AndroidDevice{
+enum class AndroidDevice {
     AndroidZXW,
     AndroidFYT,
     AndroidXT,
